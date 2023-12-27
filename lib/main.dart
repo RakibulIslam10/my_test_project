2a4d541d8b0107d5fd3bbc027370db05e61c2c6c
RAKIB <rakibulislam10.net@gmail.com>
2023-12-27 23:21:21 +0600

    whatsapp

--- lib/main.dart
+++ lib/main.dart
@@ -1,81 +1,116 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:my_test_project/WellcomePage.dart';
 
 void main() {
-  runApp(Data());
+  runApp(Whatasapp());
 }
 
-class Data extends StatelessWidget {
+class Whatasapp extends StatelessWidget {
+  var Items = [
+    {
+      "title": "Sumaiya Friend",
+      "image":
+          "https://www.mockofun.com/wp-content/uploads/2019/12/circle-photo.jpg",
+      "subtitle": "Hey! Rakib How are you?"
+    },
+    {
+      "title": "Serenity",
+      "image":
+          "https://www.mockofun.com/wp-content/uploads/2019/12/circle-photo.jpg",
+      "subtitle": " Hey! 👋 "
+    },
+    {
+      "title": "Tranquility",
+      "image":
+          "https://www.mockofun.com/wp-content/uploads/2019/12/circle-photo.jpg",
+      "subtitle": "How is it going brother?"
+    },
+    {
+      "title": "Scenic Beauty",
+      "image":
+          "https://www.mockofun.com/wp-content/uploads/2019/12/circle-photo.jpg",
+      "subtitle": "Hey Rakib are you busy??"
+    },
+    {
+      "title": "Sunset Views",
+      "image":
+          "https://www.mockofun.com/wp-content/uploads/2019/12/circle-photo.jpg",
+      "subtitle": "Sunset reflections"
+    },
+    {
+      "title": "Panoramic Horizon",
+      "image":
+          "https://www.mockofun.com/wp-content/uploads/2019/12/circle-photo.jpg",
+      "subtitle": "Peaceful horizon"
+    },
+    {
+      "title": "Majestic Scenery",
+      "image":
+          "https://www.mockofun.com/wp-content/uploads/2019/12/circle-photo.jpg",
+      "subtitle": "Majestic view"
+    },
+    {
+      "title": "Harmonious Nature",
+      "image":
+          "https://www.mockofun.com/wp-content/uploads/2019/12/circle-photo.jpg",
+      "subtitle": "Harmony of nature"
+    },
+    {
+      "title": "Idyllic Countryside",
+      "image":
+          "https://www.mockofun.com/wp-content/uploads/2019/12/circle-photo.jpg",
+      "subtitle": "Idyllic scenery"
+    },
+    {
+      "title": "Tranquil Vistas",
+      "image":
+          "https://www.mockofun.com/wp-content/uploads/2019/12/circle-photo.jpg",
+      "subtitle": "Tranquility in nature"
+    }
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MyHomePage(),
-    );
-  }
-}
+      home: DefaultTabController(
+        length: 3,
+        child: DefaultTabController(
+          length: 4,
+          child: Scaffold(
+            appBar: AppBar(
+              actions: [
+                IconButton(
+                    onPressed: () {}, icon: Icon(Icons.camera_alt_outlined)),
+                IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+                IconButton(onPressed: () {}, icon: Icon(Icons.more_vert)),
+              ],
+              title: Text("WhatsApp"),
+              backgroundColor: Colors.green,
+              bottom: TabBar(tabs: [
+                Text('Chats'),
+               Text('Status'),
 
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  TextEditingController _name = TextEditingController();
-  TextEditingController _email = TextEditingController();
-  TextEditingController _phone = TextEditingController();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Center(child: Text("Data Passing")),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(18.0),
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                controller: _name,
-                decoration: (InputDecoration(
-                    hintText: "Enter your Name", border: OutlineInputBorder())),
-              ),
+                Text('Calls'),
+              ]),
             ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                controller: _email,
-                decoration: (InputDecoration(
-                    hintText: "Enter your email",
-                    border: OutlineInputBorder())),
-              ),
+            body: ListView.builder(
+              itemCount: Items.length,
+              itemBuilder: (context, index) {
+                return InkWell(
+                  child: ListTile(
+                    title: Text(Items[index]["title"].toString(),
+                      style: TextStyle(fontWeight: FontWeight.bold),),
+                    subtitle: Text(Items[index]["subtitle"].toString(),),
+                    leading: Image.network(Items[index]["image"].toString()),
+                  ),
+                );
+              },
             ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                keyboardType: TextInputType.number,
-                controller: _phone,
-                decoration: (InputDecoration(
-                    hintText: "Enter your phone",
-                    border: OutlineInputBorder())),
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).push(MaterialPageRoute(
-                      builder: (context) => WellcomePage(
-                          name: _name.text,
-                          email: _email.text,
-                          phone: _phone.text)));
-                },
-                child: Text("Go to Next Page"))
-          ],
+            floatingActionButton: FloatingActionButton(onPressed: (){},
+            child: Icon(Icons.message),
+            backgroundColor: Colors.green,),
+          ),
         ),
       ),
     );
