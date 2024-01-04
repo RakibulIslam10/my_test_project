70a4c9458f9a8dca61228057b841d51c6392e6a9
RAKIB <rakibulislam10.net@gmail.com>
2024-01-04 16:31:33 +0600

    new changes user profile add

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'Gaget.dart';
 import 'MainPage.dart';
 import 'Medicine.dart';
 import 'favorite.dart';
-import 'fstLogin.dart';
+
 import 'notifi.dart';
 import 'profile.dart';
 import 'search.dart';
@@ -34,15 +34,8 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
-  int rakibss = 0;
-
-  List MyPage = [
-    MainPage(),
-    favorite(),
-    notifi(),
-    profilepage()
-  ];
+  int myallPagelist = 0;
+  List MyPage = [apple(),potato(),napa(),headphone(),];
 
   @override
   Widget build(BuildContext context) {
@@ -58,11 +51,11 @@ class _HomeScreenState extends State<HomeScreen> {
           backgroundColor: Colors.teal,
           title: Center(
               child: Text(
-                "My Market",
-                style: TextStyle(
-                  fontSize: 25,
-                ),
-              )),
+            "My Market",
+            style: TextStyle(
+              fontSize: 25,
+            ),
+          )),
           actions: [
             IconButton(
                 onPressed: () =>
@@ -72,7 +65,7 @@ class _HomeScreenState extends State<HomeScreen> {
             IconButton(onPressed: () {}, icon: Icon(Icons.more_vert)),
           ],
           bottom:
-          TabBar(labelPadding: EdgeInsets.symmetric(vertical: 2), tabs: [
+              TabBar(labelPadding: EdgeInsets.symmetric(vertical: 2), tabs: [
             Tab(
               icon: Icon(Icons.shopify_rounded),
               text: "Fruits",
@@ -93,101 +86,106 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
         drawer: Drawer(
             child: ListView(
-              children: [
-                DrawerHeader(
-                  padding: EdgeInsets.all(0),
-                  child: Container(
-                    child: Column(
-                      children: [
-                        SizedBox(
-                            height: 110,
-                            child: Padding(
-                              padding: const EdgeInsets.only(top: 15.0),
-                              child: Image.network(
-                                "https://cdn-icons-png.flaticon.com/256/9368/9368192.png",
-                              ),
-                            )),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text(
-                            "👋 Hey there!",
-                            style: TextStyle(fontSize: 25, color: Colors.white),
+          children: [
+            DrawerHeader(
+              padding: EdgeInsets.all(0),
+              child: Container(
+                child: Column(
+                  children: [
+                    SizedBox(
+                        height: 110,
+                        child: Padding(
+                          padding: const EdgeInsets.only(top: 15.0),
+                          child: Image.network(
+                            "https://cdn-icons-png.flaticon.com/256/9368/9368192.png",
                           ),
-                        )
-                      ],
-                    ),
-                    color: Colors.teal,
-                  ),
-                ),
-                ListTile(
-                  leading: Icon(Icons.home),
-                  title: Text("Home"),
-                  onTap: () {},
-                ),
-                ListTile(
-                  leading: Icon(Icons.person),
-                  title: Text("Profile"),
-                  onTap: () {},
-                ),
-                ListTile(
-                  leading: Icon(Icons.settings),
-                  title: Text("Setting"),
-                  onTap: () {},
-                ),
-                ListTile(
-                  leading: Icon(Icons.share),
-                  title: Text("Share"),
-                  onTap: () {},
-                ),
-                ListTile(
-                  leading: Icon(Icons.star),
-                  title: Text("Rate App"),
-                  onTap: () {},
-                ),
-                ListTile(
-                  leading: Icon(Icons.email_rounded),
-                  title: Text("Contact Us"),
-                  onTap: () {},
-                ),
-                ListTile(
-                  leading: Icon(Icons.lock),
-                  title: Text("Privacy Policy"),
-                  onTap: () {},
-                ),
-                ListTile(
-                  leading: Icon(Icons.dark_mode_outlined),
-                  title: Text("Dark Mode"),
-                  onTap: () {},
-                ),
-                ListTile(
-                  leading: Icon(Icons.exit_to_app),
-                  title: Text("Exit"),
-                  onTap: () {},
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 120.0),
-                  child: Padding(
-                    padding: const EdgeInsets.only(top: 48.0),
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: .0),
-                      child: ElevatedButton(
-                        onPressed: () {},
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.teal,
-                        ),
-                        child: Text("Exit"),
+                        )),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        "👋 Hey there!",
+                        style: TextStyle(fontSize: 25, color: Colors.white),
                       ),
+                    )
+                  ],
+                ),
+                color: Colors.teal,
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text("Home"),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text("Profile"),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text("Setting"),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.share),
+              title: Text("Share"),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.star),
+              title: Text("Rate App"),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.email_rounded),
+              title: Text("Contact Us"),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.lock),
+              title: Text("Privacy Policy"),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.dark_mode_outlined),
+              title: Text("Dark Mode"),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.exit_to_app),
+              title: Text("Exit"),
+              onTap: () {},
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 120.0),
+              child: Padding(
+                padding: const EdgeInsets.only(top: 48.0),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: .0),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.teal,
                     ),
+                    child: Text("Exit"),
                   ),
-                )
-              ],
-            )),
-        body: MyPage[rakibss],
+                ),
+              ),
+            )
+          ],
+        )),
+body: MyPage[myallPagelist],
         bottomNavigationBar: BottomNavigationBar(
-          currentIndex: rakibss,
-          onTap: (indexbb){
-            rakibss = indexbb;
-            setState(() {});
+          currentIndex: 0,
+          onTap: (value) {
+            if (value == 3) {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => profilepage(),
+                  ));
+            }
           },
 
           type: BottomNavigationBarType.fixed,
--- lib/profile.dart
+++ lib/profile.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class profilepage extends StatelessWidget {
@@ -5,8 +6,66 @@ class profilepage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return
-      Center(child: Text("My Profile 👋 ",
-      style: TextStyle(fontSize: 32,fontWeight: FontWeight.bold),));
+
+    return Scaffold(
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 18.0),
+            child: Column(
+              children: [
+                const SizedBox(height: 70,),
+
+                const CircleAvatar(
+                    radius: 70,
+                    backgroundImage: NetworkImage("https://shorturl.at/quEIO")),
+
+                const SizedBox(height: 15,),
+                // ListTile use for name and subtitle and others under the picture
+                ProfileAllItems("Name", "Rakibul Islam", CupertinoIcons.person),
+                SizedBox(height: 10,),
+                ProfileAllItems("Phone", "01987845068", CupertinoIcons.phone),
+                SizedBox(height: 10,),
+                ProfileAllItems("Address", "Mannan nogar,trash,sirajganj,", CupertinoIcons.location),
+                SizedBox(height: 10,),
+                ProfileAllItems("Email", "mdrakibulislam10.net@gmail.com", CupertinoIcons.mail),
+                SizedBox(height: 10,),
+                ProfileAllItems("Security", "Password", CupertinoIcons.lock),
+                SizedBox(height: 35,),
+                OutlinedButton(onPressed: (){}, child: Text("Edit Profile"),
+                  style: ElevatedButton.styleFrom(
+                      textStyle: TextStyle(fontSize: 15),
+                      minimumSize: Size(60, 50)
+                  ),)
+
+
+
+              ],
+            ),
+          ),
+        )
+    );
+}
+  ProfileAllItems(String title,String subtitle ,IconData iconData) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(offset: Offset(0, 5),
+              color: Colors.green.withOpacity(.2),
+              blurRadius: 10,
+              spreadRadius: 3),
+        ],
+        borderRadius: BorderRadius.circular(13),
+
+      ),
+
+      child: ListTile(
+        title: Text(title),
+        subtitle: Text(subtitle),
+        leading: Icon(iconData),
+        trailing: Icon(CupertinoIcons.arrow_right),
+        tileColor: Colors.white,
+      ),
+    );
   }
 }
